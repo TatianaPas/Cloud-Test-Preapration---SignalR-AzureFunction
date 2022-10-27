b86cc9d9a11e3a5263e946ab53db000570d57b04
Tatiana <tatjana.pasechnik@gmail.com>
2022-10-28 12:38:00 +1300

    visits count, users count

---
+++ SignalRSamlple/SignalRSamlple/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalRSamlple.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SignalRSamlple/SignalRSamlple/wwwroot/js/UsersCount.js
@@ -0,0 +1,36 @@
+﻿// create connection
+
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//connect to methods that hub invokes, receive notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+//invoke hub methods, send notifiction to hub
+
+function NewWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+
+//start connection
+
+function fulfilled() {
+    console.log("Connection to User HubSuccessfull");
+    NewWindowLoadedOnClient();
+
+}
+
+function rejected() {
+    alert("Connection to User Hub Rejected");
+}
+
+
+connectionUserCount.start().then(fulfilled, rejected);
